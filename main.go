401f13d2a6afae8dc348b730764898c691939262
Jason Kwon <jasonykwon91@gmail.com>
2016-09-26 18:05:35 -0500

    asdjfnadjksbf

--- main.go
+++ main.go
@@ -14,6 +14,7 @@ import (
     "strings"
     "errors"
     "strconv"
+    "reflect"
 
     "github.com/ContinuumLLC/BBS/r1util"
 )
@@ -31,12 +32,12 @@ type Key struct {
 }
 
 type Job struct {
-    Name string `json:"name"`
-    Builds []int64 `json:"builds"`
+    Name string `json:"Name"`
+    Builds []int64 `json:"Builds"`
 }
 
 type JobHistory struct {
-    Name string `json:"Name"`
+    JobName string `json:"JobName"`
     BuildHistory []Build `json:"BuildHistory"`
 }
 
@@ -57,6 +58,9 @@ type Build struct {
 type Artifact struct {
     Name string
     Url string
+    Job string
+    Version string
+    Build string
 }
 
 func startRouter(port int) {
@@ -67,41 +71,177 @@ func startRouter(port int) {
 
     router.AddRoute("GET", "/rest/auth", authHandler)
     router.AddRoute("GET", "/rest/systems", systemsHandler)
+    router.AddRoute("GET", "/rest/systems/:system", systemsPackagesHandler)
     router.AddRoute("GET", "/rest/artifacts/initialize", initializeArtifactsHandler)
     router.AddRoute("GET", "/rest/artifacts", artifactsHandler)
     router.AddRoute("GET", "/rest/system/artifacts/initialize", initializeSystemArtifactsHandler)
     router.AddRoute("GET", "/rest/:system/artifacts", systemArtifactsHandler)
 
+    //really need to get rid of these eventually
+    router.AddRoute("GET", "/rest/jobs", jobsHandler)
+    router.AddRoute("GET", "/rest/jobs/:job", jobBuildHandler)
+    router.AddRoute("GET", "/rest/jobs/:job/:build" jobBuildHandler)
+
+    router.AddRoute("POST", "/rest/build", initializeBuildHandler)
+
+    router.AddRoute("GET", "/rest/test", testHandler)
+
+    setSettings()
     router.Run()
+}
+
+var settings Settings
+
+type Settings struct {
+    BuildMasters []string `json:"BuildMasters"`
+    JobFilters []string `json:"JobFilters"`
+    Systems []string `json:"Systems"`
+}
+
+func setSettings() {
+    b, err := ioutil.ReadFile("/home/jkwon/Git/releaseBuilder/settings.json")
+    if err != nil {
+        fmt.Println(err)
+    }
+       
+    err = json.Unmarshal(b, &settings)
+    if err != nil {
+        fmt.Println(err)
+    }
+}
+
+func jobsHandler(resp http.ResponseWriter, req *http.Request, parsedURL map[string]string) *r1util.AppError {
+
+    b, err := json.Marshal(settings.JobFilters)
+    if err != nil {
+        return &r1util.AppError{err, "Error sending jobs: " + err.Error(), 500}
+    }
+
+    resp.Header().Set("Content-Type", "application/json")
+    resp.Write(b)
+    return nil
+}
+
+func jobBuildHandler(resp http.ResponseWriter, req *http.Request, parsedURL map[string]string) *r1util.AppError {
+
+    job := parsedURL["job"]
+    var port string
+    var buildServer string
+    
+    var jobObj Job
+    jobObj.Name = job
+    
+
+    isSbmJob := strings.Contains(job, "ServerBackup")
+    if isSbmJob {
+        buildServer = "sbci.do.r1soft.com"
+        port = "8080"
+    } else {
+        buildServer = "ci.do.r1soft.com"
+        port = ""
+    }
+
+    client, err := CreateRestClient(buildServer, port)
+    if err != nil {
+        r1util.LogError("Failed to create rest client")
+        return &r1util.AppError{err, "Failed to create rest client: " + err.Error(), 500}
+    } 
+
+    result, err := client.Execute("/job/" + job + "/api/json", "GET")
+    if err != nil {
+        r1util.LogError("Failed to execute rest call")
+        return &r1util.AppError{err, "Failed to execute rest call: " + err.Error(), 500}
+    }
+
+    var jobDetails map[string]interface{}
+    err = json.Unmarshal(result, &jobDetails)
+
+    // configure this in case we need to use windows and linux 32 and 64 bit, right now it
+    // only uses 64 bit
+    if isSbmJob {
+        var buildNumList []int64
+        activeConfigurations := reflect.ValueOf(jobDetails["activeConfigurations"])
+
+        for i:=0; i<activeConfigurations.Len(); i++ {
+            var tmp interface{} = activeConfigurations.Index(i).Interface()
+            configList, ok := tmp.(map[string]interface{})
+            if ok {
+                config := configList["name"].(string)
+                if strings.Contains(config, "linux-64") {
+
+                    result, err := client.Execute("/job/" + job +"/" + config + "/api/json", "GET")
+                    if err != nil {
+                        r1util.LogError("Failed to execute rest call")
+                        return &r1util.AppError{err, "Failed to execute rest call: " + err.Error(), 500}
+                    }
+
+                    var hello map[string]interface{}
+                    err = json.Unmarshal(result, &hello)
+
+                    tmpBuildList := reflect.ValueOf(hello["builds"])
+                    for j:=0; j<tmpBuildList.Len(); j++ {
+                        tmp = tmpBuildList.Index(j).Interface()
+                        buildList, ok := tmp.(map[string]interface{})
+                        if ok {
+                            // don't ask me why...fix it later
+                            floatNum := strconv.FormatFloat(buildList["number"].(float64), 'f', -1, 64)
+                            intNum, _ := strconv.ParseInt(floatNum, 10, 64)
+                            buildNumList = append(buildNumList, intNum)
+                        }
+                    }
+                }
+            }
+        }
+        jobObj.Builds = buildNumList
+    }
+
+    bytes, err := json.Marshal(jobObj)
+    if err != nil {
+        r1util.LogError("Failed to create JSON for complete job history")
+        return &r1util.AppError{err, "Failed to create JSON for complete job history: " + err.Error(), 500}
+    }
+
+    resp.Header().Set("Content-Type", "application/json")
+    resp.Write(bytes)
+    
+    return nil
 }
 
 func CreateRestClient(hostname string, port string) (RestClient, error) {
     var retVal RestClient
 
     retVal.Hostname = hostname
-    retVal.Port = port
+    if port != "" {
+        retVal.Port = port
+    }
 
     return retVal, nil
 }
 
 func (r RestClient) Execute(restUrl string, method string) ([]byte, error) {
     client := new(http.Client)
-    req, err := http.NewRequest(method, "http://" + r.Hostname + ":" + r.Port + restUrl, nil)
+    req, err := http.NewRequest("", "", nil)
+    if r.Port != "" {
+        req, err = http.NewRequest(method, "http://" + r.Hostname + ":" + r.Port + restUrl, nil)
+    } else {
+        req, err = http.NewRequest(method, "http://" + r.Hostname + restUrl, nil)
+    }
+
     if err != nil {
-            r1util.LogError("Failed to create request: " + err.Error())
-            return nil, errors.New("Failed to create request: " + err.Error())
+        r1util.LogError("Failed to create request: " + err.Error())
+        return nil, errors.New("Failed to create request: " + err.Error())
     }
     resp, err := client.Do(req)
     if err != nil {
-            r1util.LogError("Failed to execute rest call: " + err.Error())
-            return nil, err
+        r1util.LogError("Failed to execute rest call: " + err.Error())
+        return nil, err
     }
     defer resp.Body.Close()
 
     bytes, err := ioutil.ReadAll(resp.Body)
     if err != nil {
-            r1util.LogError("Failed to execute rest call: " + err.Error())
-            return nil, errors.New("Failed to execute rest call: " + err.Error())
+        r1util.LogError("Failed to execute rest call: " + err.Error())
+        return nil, errors.New("Failed to execute rest call: " + err.Error())
     }
     return bytes, nil
 }
@@ -143,14 +283,14 @@ func staticHandler(resp http.ResponseWriter, req *http.Request, parsedURL map[st
     // We are ignoring the first char as it always starts with "/" for requests. But we don't
     // have "/" in data packed with "gopack.pl" tool.
     if data, ok = static_data[path[1:]]; !ok {
-            r1util.LogError("Could not find static file from cache: " + path)
-            http.NotFound(resp, req)
-            return nil
+        r1util.LogError("Could not find static file from cache: " + path)
+        http.NotFound(resp, req)
+        return nil
     }
 
     media_type = mime.TypeByExtension(filepath.Ext(path))
     if media_type == "" {
-            media_type = http.DetectContentType(data)
+        media_type = http.DetectContentType(data)
     }
 
     resp.Header().Set("Content-Type", media_type)
@@ -175,7 +315,7 @@ func authHandler(resp http.ResponseWriter, req *http.Request, parsedURL map[stri
 
     b, err := ioutil.ReadFile("/home/jkwon/Git/BBS/sdLite/keys.json")
     if err != nil {
-            return &r1util.AppError{err, "Error getting user: " + err.Error(), 500}
+        return &r1util.AppError{err, "Error getting user: " + err.Error(), 500}
     }
 
     var keys []Key
@@ -206,6 +346,27 @@ func systemsHandler(resp http.ResponseWriter, req *http.Request, parsedURL map[s
     }
 
     result, err := client.Execute("/systems", "GET")
+    if err != nil {
+        r1util.LogError("Failed to execute rest call")
+        return &r1util.AppError{err, "Failed to execute rest call: " + err.Error(), 500}
+    }
+
+    resp.Header().Set("Content-Type", "application/json")
+    resp.Write(result)
+    return nil
+}
+
+func systemsPackagesHandler(resp http.ResponseWriter, req *http.Request, parsedURL map[string]string) *r1util.AppError {
+
+    system := parsedURL["system"]
+
+    client, err := CreateRestClient("10.80.199.100", "9292")
+    if err != nil {
+        r1util.LogError("Failed to create rest client")
+        return &r1util.AppError{err, "Failed to create rest client: " + err.Error(), 500}
+    }
+
+    result, err := client.Execute("/systems/" + system, "GET")
     if err != nil {
         r1util.LogError("Failed to execute rest call")
         return &r1util.AppError{err, "Failed to execute rest call: " + err.Error(), 500}
@@ -257,7 +418,7 @@ func initializeArtifactsHandler(resp http.ResponseWriter, req *http.Request, par
         var jobObj Job 
         json.Unmarshal(result, &jobObj)
 
-        jobHistory.Name = jobObj.Name
+        jobHistory.JobName = jobObj.Name
 
         for _, build := range jobObj.Builds {
             buildNum := strconv.FormatInt(build, 10)
@@ -276,24 +437,9 @@ func initializeArtifactsHandler(resp http.ResponseWriter, req *http.Request, par
 
             for _, artifact := range jenkinsBuildObj.Artifacts {
                 var artifactObj Artifact
-                splitUrl := strings.Split(artifact, "/")
-                tmpArtifact := splitUrl[len(splitUrl)-1]
-                newTmpArtifact := strings.Replace(tmpArtifact, ".deb", "", -1)
-                splitArtifactName := strings.Split(newTmpArtifact, "-")
-                
-                // artifact name without '.deb', '-', '/', and job/version
-                splitArtifactName = splitArtifactName[:len(splitArtifactName)-1] 
-                var artifactName string
-                for i:=0; i<len(splitArtifactName); i++ {
-                    if i == 0 {
-                        artifactName = splitArtifactName[i]
-                    } else {
-                        artifactName = artifactName + "-" + splitArtifactName[i]
-                    }
-                }
 
-                artifactObj.Name = artifactName
-                artifactObj.Url = artifact
+                artifactObj = createArtifactObject(buildNum, artifact, job) 
+
                 artifactList = append(artifactList, artifactObj)
             }
 
@@ -313,15 +459,13 @@ func initializeArtifactsHandler(resp http.ResponseWriter, req *http.Request, par
             return &r1util.AppError{err, "Failed to create JSON for job history: " + err.Error(), 500}
         }
  
-        fileName := "/home/jkwon/Git/releaseBuilder/BuildHistory/" + jobHistory.Name + ".BuildHistory.json"
+        fileName := "/home/jkwon/Git/releaseBuilder/BuildHistory/" + jobHistory.JobName + ".BuildHistory.json"
         err = ioutil.WriteFile(fileName, bytes, 0644)
         if err != nil {
             r1util.LogError("Failed to create JSON for job history")
             return &r1util.AppError{err, "Failed to create JSON for job history: " + err.Error(), 500}
         }
     }
-
-    fmt.Printf("%v", jobHistoryList)
 
     bytes, err := json.Marshal(jobHistoryList)
     if err != nil {
@@ -340,6 +484,73 @@ func initializeArtifactsHandler(resp http.ResponseWriter, req *http.Request, par
     resp.Write(result)
     return nil
 }
+
+func createArtifactObject(buildNum string, artifactUrl string, jobName string) (Artifact) {
+    splitUrl := strings.Split(artifactUrl, "/")
+    // only artifact name
+    tmpArtifact := splitUrl[len(splitUrl)-1]
+    // strip '.deb'
+    newTmpArtifact := strings.Replace(tmpArtifact, ".deb", "", -1)
+   
+    var tmpArtObj Artifact
+
+    sbm := [2]string{"ServerBackup-6.0.x", "ServerBackup-6.2.x"}
+
+    isSbm := false
+    for i:=0; i<2; i++ {
+        if strings.EqualFold(jobName, sbm[i]) {
+            isSbm = true
+        }
+    } 
+
+    //if (strings.Contains(jobName, "ServerBackup")) {
+    if (isSbm) {
+        // artifact name without '.deb', '-', '/'
+        splitArtifactName := strings.Split(newTmpArtifact, "-")
+        
+        if splitArtifactName[0] == "idera" {
+            tmpArtObj.Name = splitArtifactName[0] + "-" + splitArtifactName[1]
+            tmpArtObj.Build = buildNum
+            tmpArtObj.Version = splitArtifactName[3] + "-" + buildNum
+        } else if splitArtifactName[1] == "docstore" {
+            tmpArtObj.Build = buildNum
+            tmpArtObj.Name = splitArtifactName[0] + "-" + splitArtifactName[1]
+            tmpArtObj.Version = splitArtifactName[2] + "-" + buildNum
+        } else {
+            tmpArtObj.Build = splitArtifactName[len(splitArtifactName)-1]
+            tmpArtObj.Version = splitArtifactName[len(splitArtifactName)-2] + "-" + tmpArtObj.Build
+            splitArtifactName = splitArtifactName[:len(splitArtifactName)-3]
+            for i:=0; i<len(splitArtifactName); i++ {
+                if i == 0 {
+                    tmpArtObj.Name = splitArtifactName[i]
+                } else {
+                    tmpArtObj.Name = tmpArtObj.Name + "-" + splitArtifactName[i]
+                }
+            }
+        }
+    } else /*if other jobs i.e. BBS, c247, r1scsi*/ {
+        // artifact name without '.deb', '_', '/'
+        if (strings.Contains(newTmpArtifact,"c247tools") || strings.Contains(newTmpArtifact,"c247mon")) {
+            splitArtifactName := strings.Split(newTmpArtifact, "-")
+            tmpArtObj.Name = splitArtifactName[0]
+            tmpArtObj.Build = buildNum
+            tmpArtObj.Version = splitArtifactName[1] + "-" + buildNum
+        } else {
+            splitArtifactName := strings.Split(newTmpArtifact, "_")
+            tmpArtObj.Name = splitArtifactName[0]
+            tmpArtObj.Version = splitArtifactName[1]
+            tmpString := strings.Split(splitArtifactName[1], "-")
+            tmpArtObj.Build = tmpString[1]
+        }
+    }
+    
+    tmpArtObj.Job = jobName
+    tmpArtObj.Url = artifactUrl
+
+    return tmpArtObj
+
+}
+
 
 func artifactsHandler(resp http.ResponseWriter, req *http.Request, parsedURL map[string]string) *r1util.AppError {
     
@@ -376,6 +587,10 @@ func initializeSystemArtifactsHandler(resp http.ResponseWriter, req *http.Reques
     
     var systemList []string = make([]string, 0, 0)
     err = json.Unmarshal(result, &systemList)
+    if err != nil {
+        r1util.LogError("Failed to create system list")
+        return &r1util.AppError{err, "Failed to create system list: " + err.Error(), 500}
+    }
     
     var jobHistoryList []JobHistory 
 
@@ -387,12 +602,109 @@ func initializeSystemArtifactsHandler(resp http.ResponseWriter, req *http.Reques
 
     json.Unmarshal(bytes, &jobHistoryList)
 
-    
+    var artifactList []Artifact
+
+    for _, system := range systemList {
+        result, err := client.Execute("/systems/" + system, "GET")
+        if err != nil {
+            r1util.LogError("Failed to execute rest call")
+            return &r1util.AppError{err, "Failed to execute rest call: " + err.Error(), 500}
+        }
+
+        var sysPackageList []string = make([]string, 0, 0)
+        err = json.Unmarshal(result, &sysPackageList)
+        if err != nil {
+            r1util.LogError("Failed to system package list")
+            return &r1util.AppError{err, "Failed to create system package list: " + err.Error(), 500}
+        }
+
+        for _, sysPackage := range sysPackageList {
+            for _, job := range jobHistoryList {
+                for _, buildHistory := range job.BuildHistory {
+                    for _, artifact := range buildHistory.Artifacts {
+                        if (strings.EqualFold(artifact.Name, sysPackage)) {
+                            //fmt.Println(sysPackage + " " + job.JobName + " "+ artifact.Name + " " + artifact.Job + " " + artifact.Version)
+                            tmp := exists(artifactList, artifact)
+                            if !tmp {
+                                //fmt.Println(sysPackage + " " + job.JobName + " "+ artifact.Name + " " + artifact.Job + " " + artifact.Version)
+                                artifactList = append(artifactList, artifact)
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        
+        bytes, err := json.Marshal(artifactList)
+        if err != nil {
+            r1util.LogError("Failed to create JSON for system artifact list")
+            return &r1util.AppError{err, "Failed to create JSON for system artifact list: " + err.Error(), 500}
+        }
+        fileName := "/home/jkwon/Git/releaseBuilder/BuildHistory/" + system + ".Artifacts.json"
+        err = ioutil.WriteFile(fileName, bytes, 0644)
+    }
 
     return nil
 }
 
+func exists(artifactList []Artifact, artifact Artifact) bool {
+    for _, tmpArt := range artifactList {
+        // need to ask about this one.....
+        /*
+        if (strings.Contains(artifact.Job, "c247ufw-master") && strings.Contains(tmpArt.Job, "c247ufw-master") ) {
+            if (artifact.Version == tmpArt.Version) {
+                return true
+            }
+        }
+        */
+        if tmpArt == artifact {
+            return true
+        }
+    }
+
+    return false
+}
+
 func systemArtifactsHandler(resp http.ResponseWriter, req *http.Request, parsedURL map[string]string) *r1util.AppError {
+
+    var artifactList []Artifact
+    system := parsedURL["system"]
+
+    bytes, err := ioutil.ReadFile("/home/jkwon/Git/releaseBuilder/BuildHistory/" + system + ".Artifacts.json")
+    if err != nil {
+        r1util.LogError("Failed to read system artifacts list")
+        return &r1util.AppError{err, "Failed to read system artifacts list: " + err.Error(), 500}
+    }
+
+    json.Unmarshal(bytes, &artifactList)
+
+    resp.Header().Set("Content-Type", "application/json")
+    resp.Header().Set("Access-Control-Allow-Origin", "*")
+    resp.Write(bytes)
+    
+    return nil
+}
+
+func initializeBuildHandler(resp http.ResponseWriter, req *http.Request, parsedURL map[string]string) *r1util.AppError {
+    fmt.Printf("%v", req);
+    bytes, err := ioutil.ReadAll(req.Body)
+    if err != nil {
+        r1util.LogError("Failed to execute rest call: " + err.Error())
+    }
+    fmt.Printf("%v", string(bytes))
+    /*
+    client, err := CreateRestClient("10.80.199.100", "9292")
+    if err != nil {
+        r1util.LogError("Failed to create rest client")
+        return &r1util.AppError{err, "Failed to create rest client: " + err.Error(), 500}
+    }
+
+    result, err := client.Execute("/systems", "GET")
+    if err != nil {
+        r1util.LogError("Failed to execute rest call")
+        return &r1util.AppError{err, "Failed to execute rest call: " + err.Error(), 500}
+    }
+    */
     return nil
 }
 
